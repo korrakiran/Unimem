b0c97b7e98215068250182a2eab0a2678205e642
Kiran Korra <kirankorra831@gmail.com>
2026-06-11 10:21:18 +0530

    Update unimem.rb

--- brew/unimem.rb
+++ brew/unimem.rb
@@ -10,7 +10,7 @@ class Unimem < Formula
 
   desc "Universal Project Memory Layer for AI Coding Agents"
   homepage "https://github.com/korrakiran/unimem"
-  url "https://github.com/korrakiran/collector/archive/refs/tags/v0.4.0.tar.gz"
+  url "https://github.com/korrakiran/unimem/archive/refs/tags/v0.4.0.tar.gz"
   sha256 "33157f6b92697883a76601ed3f5c2202fbfaa84445efe2f0ebdf16de0d8c2a83"
   license "MIT"
   head "https://github.com/korrakiran/unimem.git", branch: "main"
